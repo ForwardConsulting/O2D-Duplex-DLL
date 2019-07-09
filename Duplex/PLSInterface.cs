056ef936b52860490f2f1cc697e9745b5479f075
yuthapow <yuthapong@forward-consult.com>
2019-07-09 07:54:43 +0700

    create PLS Interface

---
+++ Duplex/PLSInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Duplex
+{
+    class PLSInterface
+    {
+        private DataTable _DTOrderItemOpr;
+
+        public PLSInterface(DataTable DTOrderItemOperation )
+        {
+            DTOrderItemOpr = DTOrderItemOperation;
+        }
+
+        public DataTable DTOrderItemOpr{ get => _DTOrderItemOpr; set => _DTOrderItemOpr = value; }
+
+        
+    }
+}
